32b1b9752a799fa4c6990e20f21422236525d0d3
erik <erikness@nmsu.edu>
2014-11-04 10:04:12 -0700

    Updated, simpler symbol scheme that's basically a hacked version of str

--- zipline/futures/symbol.py
+++ zipline/futures/symbol.py
@@ -5,61 +5,56 @@ object instead, one that not only has a string representation but some contextua
 "what kind of security is this?"
 """
 
-from zipline.futures.utils import date_from_month_code, month_code_from_date
+class Symbol(str):
+    def __new__(cls, base, asset_type="stock", month_code=None):
+        """This override necessary so __init__ accepts a third parameter"""
+        return str.__new__(cls, base, month_code)
 
-class Security(object):
-    def as_symbol(self):
-        """Returns the parts of the object needed to find data relevant to it within a DataSource.
-        For example, data sources may return a futures contract like so: "FG.N10".
-        If so, this method is meant to return the string representation of the contract so it can be indexed."""
-        raise NotImplementedError()
+    def __init__(self, base, asset_type="stock"):
+        """Possible values for asset_type: stock, underlying, contract"""
+        self.asset_type = asset_type
+        str.__init__(self, base)
 
-class Stock(Security):
-    """This class should act like a string as much as possible. Stocks really do not need context."""
-    def __init__(self, symbol):
-        self.symbol = symbol
+    def __str__(self):
+        return self
 
-    def as_symbol(self):
-        return self.symbol
+    def __add__(self, other):
+        # keep left asset type unless only the right object is a Symbol
+        return Symbol(str.__add__(self, other), self.asset_type)
 
+    def __radd__(self, other):
+        # invoked when adding string + symbol
+        return Symbol(other) + self
 
-class FuturesContract(Security):
-    """This class should act like a string, with the exception of a forced sell when the algorithm hits its
-    expiry date.
+"""Test:
 
-    Expiration should be a month code, though it will be stored as a date. Expiry, if provided should be
-    a datetime."""
-    def __init__(self, underlying, month_code, expiry=None):
-        self.underlying = underlying
-        if expiry is not None:
-            # we have information other than the month code from our data
-            self.expiry = expiry
-        else:
-            self.expiry = date_from_month_code(month_code)
+S = Symbol("XOM", asset_type="underlying")
 
-    def as_symbol(self):
-        return self.underlying + month_code_from_date(self.expiry)
+# Load data manually from Yahoo! finance
+start = datetime(2005, 1, 1, 0, 0, 0, 0, pytz.utc)
+end = datetime(2012, 1, 1, 0, 0, 0, 0, pytz.utc)
+data = load_bars_from_yahoo(stocks=[S], start=start,
+                            end=end)
 
-class FuturesUnderlying(Security):
-    """This class should roughly act as a string, but track a different contract every time the previous one expires."""
-    def __init__(self, underlying):
-        self.underlying = underlying
-        self.current_contract = None  # when this is set, it will be a month code
+# Define algorithm
+def initialize(context):
+    pass
 
-    def as_symbol(self):
-        return self.underlying + self.current_contract
+def handle_data(context, data):
+    order(S, 10)
+    record(**{S: data[S].price})
 
+# Create algorithm object passing in initialize and
+# handle_data functions
+algo_obj = TradingAlgorithm(initialize=initialize,
+                            handle_data=handle_data)
 
-def assign_symbol(stock=None, underlying=None, month_code=None, expiry=None):
-    """Expiry, if present, should be a datetime. Everything else should be a string.
+# Run algorithm
+perf_manual = algo_obj.run(data)
 
-    Returns some subclass of Security."""
-    if stock is not None:
-        return Stock(stock)
-    elif underlying is not None:
-        if month_code is not None:
-            return FuturesContract(underlying, month_code, expiry)
-        else:
-            return FuturesUnderlying(underlying)
-    else:
-        raise ValueError("You must provide either a stock symbol or an underlying futures symbol.")
+ax1 = plt.subplot(211)
+perf_manual.portfolio_value.plot(ax=ax1)
+ax1.set_ylabel('portfolio value')
+ax2 = plt.subplot(212, sharex=ax1)
+perf_manual[S].plot(ax=ax2)
+ax2.set_ylabel(S + ' stock price')"""
